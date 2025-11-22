e67968dd7cc03df794058a7d0b670971bab01f3f
tanmengyang <1940789311@qq.com>
2025-11-22 22:17:31 +0800

    feat:交易结算责任链过滤

---
+++ group-buy-market-domain/src/main/java/cn/bugstack/domain/trade/service/settlement/TradeSettlementOrderService.java
@@ -0,0 +1,77 @@
+package cn.bugstack.domain.trade.service.settlement;
+
+import cn.bugstack.domain.trade.adapter.repository.ITradeRepository;
+import cn.bugstack.domain.trade.model.aggregate.GroupBuyTeamSettlementAggregate;
+import cn.bugstack.domain.trade.model.entity.*;
+import cn.bugstack.domain.trade.service.ITradeSettlementOrderService;
+import cn.bugstack.domain.trade.service.settlement.factory.TradeSettlementRuleFilterFactory;
+import cn.bugstack.types.design.framework.link.model2.chain.BusinessLinkedList;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+/**
+ * @author Fuzhengwei bugstack.cn @小傅哥
+ * @description 拼团交易结算服务
+ * @create 2025-01-26 15:22
+ */
+@Slf4j
+@Service
+public class TradeSettlementOrderService implements ITradeSettlementOrderService {
+
+    @Resource
+    private ITradeRepository repository;
+    @Resource
+    private BusinessLinkedList<TradeSettlementRuleCommandEntity, TradeSettlementRuleFilterFactory.DynamicContext, TradeSettlementRuleFilterBackEntity> tradeSettlementRuleFilter;
+
+    @Override
+    public TradePaySettlementEntity settlementMarketPayOrder(TradePaySuccessEntity tradePaySuccessEntity) throws Exception {
+        log.info("拼团交易-支付订单结算:{} outTradeNo:{}", tradePaySuccessEntity.getUserId(), tradePaySuccessEntity.getOutTradeNo());
+        // 1. 结算规则过滤
+        TradeSettlementRuleFilterBackEntity tradeSettlementRuleFilterBackEntity = tradeSettlementRuleFilter.apply(
+                TradeSettlementRuleCommandEntity.builder()
+                        .source(tradePaySuccessEntity.getSource())
+                        .channel(tradePaySuccessEntity.getChannel())
+                        .userId(tradePaySuccessEntity.getUserId())
+                        .outTradeNo(tradePaySuccessEntity.getOutTradeNo())
+                        .outTradeTime(tradePaySuccessEntity.getOutTradeTime())
+                        .build(),
+                new TradeSettlementRuleFilterFactory.DynamicContext());
+
+        String teamId = tradeSettlementRuleFilterBackEntity.getTeamId();
+
+        // 2. 查询组团信息
+        GroupBuyTeamEntity groupBuyTeamEntity = GroupBuyTeamEntity.builder()
+                .teamId(tradeSettlementRuleFilterBackEntity.getTeamId())
+                .activityId(tradeSettlementRuleFilterBackEntity.getActivityId())
+                .targetCount(tradeSettlementRuleFilterBackEntity.getTargetCount())
+                .completeCount(tradeSettlementRuleFilterBackEntity.getCompleteCount())
+                .lockCount(tradeSettlementRuleFilterBackEntity.getLockCount())
+                .status(tradeSettlementRuleFilterBackEntity.getStatus())
+                .validStartTime(tradeSettlementRuleFilterBackEntity.getValidStartTime())
+                .validEndTime(tradeSettlementRuleFilterBackEntity.getValidEndTime())
+                .build();
+
+        // 3. 构建聚合对象
+        GroupBuyTeamSettlementAggregate groupBuyTeamSettlementAggregate = GroupBuyTeamSettlementAggregate.builder()
+                .userEntity(UserEntity.builder().userId(tradePaySuccessEntity.getUserId()).build())
+                .groupBuyTeamEntity(groupBuyTeamEntity)
+                .tradePaySuccessEntity(tradePaySuccessEntity)
+                .build();
+
+        // 4. 拼团交易结算
+        repository.settlementMarketPayOrder(groupBuyTeamSettlementAggregate);
+
+        // 5. 返回结算信息 - 公司中开发这样的流程时候，会根据外部需要进行值的设置
+        return TradePaySettlementEntity.builder()
+                .source(tradePaySuccessEntity.getSource())
+                .channel(tradePaySuccessEntity.getChannel())
+                .userId(tradePaySuccessEntity.getUserId())
+                .teamId(teamId)
+                .activityId(groupBuyTeamEntity.getActivityId())
+                .outTradeNo(tradePaySuccessEntity.getOutTradeNo())
+                .build();
+    }
+
+}
